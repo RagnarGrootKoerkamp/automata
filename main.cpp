295dccedbe87641eaa915be86c8dba5bc5140896
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2020-07-16 14:01:51 +0200

    Print number of break sequence terms computed before infinity/contradiction was found

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ constexpr unsigned MAX_ORDER = 4;
 constexpr int MAX_LOG_ORDER  = std::countr_zero(MAX_ORDER);
 
 constexpr int FAST_DEGREE = (1 << 10);
-constexpr int FULL_DEGREE = (1 << 15);
+constexpr int FULL_DEGREE = (1 << 14);
 
 ////////////////////////
 static_assert(std::has_single_bit(MAX_ORDER));
@@ -275,8 +275,9 @@ template <int N>
 // See break_sequence.pdf for details.
 // Return true when p potentially has finite order.
 // Return false when p fails a condition and thus must have infinite order.
+// Also returns the number of terms computed.
 template <int N>
-bool check_break_sequence(const Automaton<N>& automaton, int max_deg) {
+std::pair<bool, int> check_break_sequence(const Automaton<N>& automaton, int max_deg) {
 	// We check the break sequence modulo increasing x^DEGREE to prevent doing a lot of work on the
 	// lower terms.
 
@@ -289,7 +290,7 @@ bool check_break_sequence(const Automaton<N>& automaton, int max_deg) {
 	};
 
 	PowerSeries p{0, 1};
-	auto check_degree = [&](int degree) {
+	auto check_degree = [&](int degree) -> std::pair<bool, int> {
 		// b: lower break sequence
 		// B: upper break sequence
 		std::vector<int> b, B;
@@ -297,13 +298,13 @@ bool check_break_sequence(const Automaton<N>& automaton, int max_deg) {
 		auto p = automaton.power_series(degree);
 
 		auto b0 = depth(p);
-		if(b0 == -1) return true;
+		if(b0 == -1) return {true, 0};
 
 		b.push_back(b0);
 		B.push_back(b0);
 
 		// Condition (1):
-		if(not(gcd(b0, 2) == 1)) return false;
+		if(not(gcd(b0, 2) == 1)) return {false, 1};
 
 		// Keep computing more terms of the break sequence, until we hit infinity.
 		for(int i = 1; true; ++i) {
@@ -316,30 +317,31 @@ bool check_break_sequence(const Automaton<N>& automaton, int max_deg) {
 
 			// We've hit the limit with the current modulus x^DEGREE, and all checks so far have
 			// passed.
-			if(bi == -1) return true;
+			if(bi == -1) return {true, i - 1};
 
 			b.push_back(bi);
 
 			// Condition (0): B_i must be integers.
 			// B_i = B_(i-1) + 2^-i * (b_i - b_(i-1))
-			if(not((bi - bprev) % (1 << i) == 0)) return false;
+			if(not((bi - bprev) % (1 << i) == 0)) return {false, i};
 
 			auto Bi = Bprev + (bi - bprev) / (1 << i);
 			B.push_back(Bi);
 
 			// Condition (2): B_i >= 2*B_(i-1)
-			if(not(Bi >= 2 * Bprev)) return false;
+			if(not(Bi >= 2 * Bprev)) return {false, i};
 
 			// Condition (3): if B_i > 2*B_(i-1), then gcd(2, B_i) = 1.
 			if(Bi > 2 * Bprev)
-				if(not(gcd(2, Bi) == 1)) return false;
+				if(not(gcd(2, Bi) == 1)) return {false, i};
 		}
-		return true;
+		assert(false);
 	};
 
+	std::pair<bool, int> r;
 	for(int d = 4; d <= max_deg; d *= 2)
-		if(not check_degree(d)) return false;
-	return true;
+		if(r = check_degree(d); not r.first) return r;
+	return r;
 }
 
 // Count the number of suitable automata.
@@ -373,7 +375,7 @@ void count() {
 		if(not automaton.is_minimal()) return;
 		++num_ok_automata;
 
-		if(check_break_sequence(automaton, FULL_DEGREE)) {
+		if(auto [ok, terms] = check_break_sequence(automaton, FULL_DEGREE); ok) {
 			auto p              = automaton.power_series(FULL_DEGREE);
 			auto [it, inserted] = seen_break_sequence.emplace(p, automaton);
 			if(not inserted) {
@@ -383,7 +385,8 @@ void count() {
 			} else {
 				++num_ok_break_sequence;
 				std::cout << "Found automaton with potential finite order passing the break "
-				             "sequence check:\n"
+				             "sequence check.\n"
+				          << "Computed " << terms << " terms\n"
 				          << automaton << std::endl;
 				std::cout << "PowerSeries: " << p << std::endl << std::endl << std::endl;
 			}
@@ -408,7 +411,7 @@ void count() {
 		std::cout << automaton << std::endl;
 		std::cout << "PowerSeries: " << p << std::endl << std::endl << std::endl;
 
-		assert(check_break_sequence(automaton, FULL_DEGREE));
+		assert(check_break_sequence(automaton, FULL_DEGREE).first);
 	};
 
 	// Loop over the number of 1 labels.
